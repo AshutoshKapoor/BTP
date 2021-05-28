71e9bc9937c00928d81d72f823ff98239ece60f8
ketulrudani <41946677+ketulrudani@users.noreply.github.com>
2021-05-28 12:16:12 +0530

    Form Bug Fix 1
    Fixed Errors in form 1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import * as Chartist from 'chartist';
 
 @Component({
@@ -8,143 +9,94 @@ import * as Chartist from 'chartist';
 })
 export class DashboardComponent implements OnInit {
 
+  addNewItemForm = new FormGroup({
+    componentName : new FormControl('', [
+      Validators.required,
+    ]),
+    code : new FormControl('', [
+      Validators.required,
+    ]),
+    quantity : new FormControl('', [
+      Validators.required,
+    ]),
+    lab : new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+  get courseName() { return this.addNewItemForm.get('courseName'); }
+  get code() { return this.addNewItemForm.get('code'); }
+  get quantity() { return this.addNewItemForm.get('quantity'); }
+  get lab() { return this.addNewItemForm.get('lab'); }
+
+
+  removeItemForm = new FormGroup({
+    componentCodeRIF : new FormControl('', [
+      Validators.required,
+    ]),
+    quantityRIF : new FormControl('', [
+      Validators.required,
+    ]),
+    labRIF : new FormControl('', [
+      Validators.required,
+    ]),
+    reasonRIF : new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+  get componentCodeRIF() { return this.removeItemForm.get('componentCodeRIF'); }
+  get quantityRIF() { return this.removeItemForm.get('quantityRIF'); }
+  get labRIF() { return this.removeItemForm.get('labRIF'); }
+  get reasonRIF() { return this.removeItemForm.get('reasonRIF'); }
+
+  studentIssueForm = new FormGroup({
+    componentCodeSIF : new FormControl('', [
+      Validators.required,
+    ]),
+    quantitySIF : new FormControl('', [
+      Validators.required,
+    ]),
+    labSIF : new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+  get componentCodeSIF() { return this.studentIssueForm.get('componentCodeSIF'); }
+  get quantitySIF() { return this.studentIssueForm.get('quantitySIF'); }
+  get labSIF() { return this.studentIssueForm.get('labSIF'); }
+
+  studentReturnForm = new FormGroup({
+    rollNoSRF : new FormControl('', [
+      Validators.required,
+    ]),
+    labSRF : new FormControl('', [
+      Validators.required,
+    ]),
+  });
+
+  get rollNoSRF() { return this.studentReturnForm.get('rollNoSRF'); }
+  get labSRF() { return this.studentReturnForm.get('labSRF'); }
+
+
   constructor() { }
-  startAnimationForLineChart(chart){
-      let seq: any, delays: any, durations: any;
-      seq = 0;
-      delays = 80;
-      durations = 500;
-
-      chart.on('draw', function(data) {
-        if(data.type === 'line' || data.type === 'area') {
-          data.element.animate({
-            d: {
-              begin: 600,
-              dur: 700,
-              from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-              to: data.path.clone().stringify(),
-              easing: Chartist.Svg.Easing.easeOutQuint
-            }
-          });
-        } else if(data.type === 'point') {
-              seq++;
-              data.element.animate({
-                opacity: {
-                  begin: seq * delays,
-                  dur: durations,
-                  from: 0,
-                  to: 1,
-                  easing: 'ease'
-                }
-              });
-          }
-      });
-
-      seq = 0;
-  };
-  startAnimationForBarChart(chart){
-      let seq2: any, delays2: any, durations2: any;
-
-      seq2 = 0;
-      delays2 = 80;
-      durations2 = 500;
-      chart.on('draw', function(data) {
-        if(data.type === 'bar'){
-            seq2++;
-            data.element.animate({
-              opacity: {
-                begin: seq2 * delays2,
-                dur: durations2,
-                from: 0,
-                to: 1,
-                easing: 'ease'
-              }
-            });
-        }
-      });
-
-      seq2 = 0;
-  };
-  ngOnInit() {
-      /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
-
-      const dataDailySalesChart: any = {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-              [12, 17, 7, 17, 23, 18, 38]
-          ]
-      };
-
-     const optionsDailySalesChart: any = {
-          lineSmooth: Chartist.Interpolation.cardinal({
-              tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
-      }
-
-      var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
-
-      this.startAnimationForLineChart(dailySalesChart);
+  
+  ngOnInit() { }
 
 
-      /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
-
-      const dataCompletedTasksChart: any = {
-          labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
-          series: [
-              [230, 750, 450, 300, 280, 240, 200, 190]
-          ]
-      };
-
-     const optionsCompletedTasksChart: any = {
-          lineSmooth: Chartist.Interpolation.cardinal({
-              tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
-      }
-
-      var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-
-      // start animation for the Completed Tasks Chart - Line Chart
-      this.startAnimationForLineChart(completedTasksChart);
-
-
-
-      /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
-
-      var datawebsiteViewsChart = {
-        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
-        series: [
-          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-
-        ]
-      };
-      var optionswebsiteViewsChart = {
-          axisX: {
-              showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
-      };
-      var responsiveOptions: any[] = [
-        ['screen and (max-width: 640px)', {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
-          }
-        }]
-      ];
-      var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
-
-      //start animation for the Emails Subscription Chart
-      this.startAnimationForBarChart(websiteViewsChart);
+  addNewItemSubmit() {
+    console.log(this.addNewItemForm.value)
   }
 
+  removeItemSubmit() {
+    console.log(this.removeItemForm.value)
+  }
+
+  studentIssueSubmit() {
+    console.log(this.studentIssueForm.value)
+  }
+
+  studentReturnSubmit() {
+    console.log(this.studentReturnForm.value)
+  }
 }
